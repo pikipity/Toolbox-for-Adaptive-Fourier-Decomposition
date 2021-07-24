02d001970fe3df9ba455f8a7ba07ef2ebee5ee32
Ze Wang <wangze19910407@gmail.com>
2021-07-24 17:28:26 +0800

    Update IntroAFD

--- Docs/_build/html/_sources/BuildToolbox.rst.txt
+++ Docs/_build/html/_sources/BuildToolbox.rst.txt
@@ -9,12 +9,5 @@ Matlab
 Python
 -------
 
-.. math::
-    :nowrap:
- 
-    \begin{eqnarray}
-       y    & = & ax^2 + bx + c \\
-       f(x) & = & x^2 + 2xy + y^2
-    \end{eqnarray}
 
-AABB
+
--- Docs/conf.py
+++ Docs/conf.py
@@ -28,7 +28,8 @@ author = 'Ze Wang'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.mathjax'
+    'sphinx.ext.mathjax',
+    'sphinx.ext.graphviz'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
